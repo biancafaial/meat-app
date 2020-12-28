539af92b92bae37acf382da762d13e63425e5a29
Bianca Faial <biancafaial@gmail.com>
2020-12-28 12:01:26 -0300

    consumindo conteúdo da API

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,6 +1,7 @@
 import {  Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import { MEAT_API } from "app/app.api";
+
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
@@ -17,6 +18,12 @@ export class RestaurantsService {
     restaurants(): Observable<Restaurant[]>{
         return this.http.get(`${MEAT_API}/restaurants`)
             .map(response => response.json())
+            .catch(ErrorHandler.handlerError)
+    }
+
+    restaurantById(id: string):Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map (response => response.json())
             .catch(ErrorHandler.handlerError)
     }
 
