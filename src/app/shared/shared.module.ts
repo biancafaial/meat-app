9903b865a25d843847d76990c460f52266680293
Bianca Faial <biancafaial@gmail.com>
2021-01-08 11:57:05 -0300

    realizando pré-carregamento de todos os módulos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
+import { ModuleWithProviders, NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { OrderService } from "app/order/order.service";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
 
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
@@ -27,4 +30,12 @@ import { RatingComponent } from "./rating/rating.component";
         RatingComponent]
 
 })
-export class SharedModule { }
+export class SharedModule { 
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule:SharedModule,
+            providers:[ShoppingCartService, RestaurantsService, OrderService]
+
+        }
+    }
+}
